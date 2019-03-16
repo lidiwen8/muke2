f7474f60e278a69d3aeca65bf71ecce61f006085
lidiwen8 <1632029393@qq.com>
2019-03-16 19:18:57 +0800

    更新版本，添加用户个人中心

---
+++ src/com/muke/test/DruidDataSourceFactory.java
@@ -0,0 +1,14 @@
+//package com.muke.test;
+//import javax.sql.DataSource;
+//import org.apache.ibatis.datasource.unpooled.UnpooledDataSourceFactory;
+//import com.alibaba.druid.pool.DruidDataSource;
+//public class DruidDataSourceFactory extends UnpooledDataSourceFactory {
+//
+//    public DruidDataSourceFactory() {
+//
+//        this.dataSource = new DruidDataSource();
+//
+//    }
+//
+//}
+//
